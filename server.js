f80ce01ac4d7e86b68a5421e6b3818a8508c2014
Mahmoud Mango <mahmoudawad9000@gmail.com>
2023-05-06 21:22:52 +0300

    first commit

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
-const fs = require('fs');
 const path = require('path');
 const https = require('https');
+const fs = require('fs');
 const express = require('express');
 const helmet = require('helmet');
 const passport = require('passport');
 const { Strategy } = require('passport-google-oauth20');
 const cookieSession = require('cookie-session');
-const { verify } = require('crypto');
-
 require('dotenv').config();
 
 const PORT = 3000;
@@ -25,85 +23,86 @@ const AUTH_OPTIONS = {
   clientSecret: config.CLIENT_SECRET,
 };
 
-function verifyCallback(accessToken, refreshToken, profile, done) {
-  console.log('Google profile', profile);
-  done(null, profile);
+function verifyCallBack(accessToken, refreshToken, profile, done) {
+  console.log(`google account :`, profile);
+  done(null, profile); //used in cookie
 }
 
-passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
-
-// Save the session to the cookie
+//SAVE DATA TO COOKIE
 passport.serializeUser((user, done) => {
-  done(null, user.id);
+  done(null, user.id); // with this only step we write the hal profile and read it back when cookie is send from browser in deserialize
 });
-
-// Read the session from the cookie
+//READ DATA FROM A COOKIE
 passport.deserializeUser((id, done) => {
-  // User.findById(id).then(user => {
-  //   done(null, user);
-  // });
   done(null, id);
 });
 
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallBack));
 const app = express();
-
 app.use(helmet());
-
-app.use(cookieSession({
-  name: 'session',
-  maxAge: 24 * 60 * 60 * 1000,
-  keys: [ config.COOKIE_KEY_1, config.COOKIE_KEY_2 ],
-}));
+app.use(
+  cookieSession({
+    name: 'session',
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2],
+  })
+);
 app.use(passport.initialize());
-app.use(passport.session());
-
-function checkLoggedIn(req, res, next) { 
-  console.log('Current user is:', req.user);
-  const isLoggedIn = req.isAuthenticated() && req.user;
-  if (!isLoggedIn) {
+app.use(passport.session()); // AUTHENTICATE THE SESSION BY THE KEYS / ALLOW DESERIAIZE USER TO BE CALLED WHICH ALLOW REQ.USER TO BE
+function checkLoggedIn(req, res, next) {
+  console.log('user is :', req.user); // set by pass.session middle ware and written by passport.deserialize
+  const loggedIn = req.isAuthenticated() && req.user;
+  if (!loggedIn)
     return res.status(401).json({
-      error: 'You must log in!',
+      error: 'must be looged in',
     });
-  }
   next();
 }
 
-app.get('/auth/google', 
+app.get(
+  '/auth/google',
   passport.authenticate('google', {
     scope: ['email'],
-  }));
+  })
+);
 
-app.get('/auth/google/callback', 
+app.get(
+  '/auth/google/callback',
   passport.authenticate('google', {
-    failureRedirect: '/failure',
     successRedirect: '/',
+    failureRedirect: '/failure',
     session: true,
-  }), 
+  }),
   (req, res) => {
-    console.log('Google called us back!');
+    console.log('google called us back yay');
   }
 );
 
-app.get('/auth/logout', (req, res) => {
-  req.logout(); //Removes req.user and clears any logged in session
+app.get('/failure', (req, res, next) => {
+  res.send('failed to load ');
+});
+
+app.get('/auth/logout', (req, res, next) => {
+  req.logout(); // remove req.user and remove any session or set  BASE64 coded session instead
   return res.redirect('/');
 });
 
 app.get('/secret', checkLoggedIn, (req, res) => {
-  return res.send('Your personal secret value is 42!');
-});
-
-app.get('/failure', (req, res) => {
-  return res.send('Failed to log in!');
+  return res.send('the most secret on planet earth');
 });
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-https.createServer({
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem'),
-}, app).listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
-});
+https
+  .createServer(
+    {
+      key: fs.readFileSync('key.pem'),
+      cert: fs.readFileSync('cert.pem'),
+    },
+    app
+  )
+  .listen(PORT, () => {
+    console.log(`server is on port : ${PORT}`);
+  });
